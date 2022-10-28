c08cc3c8b8ad021eabba709d512b3c1a082c3c4b
Nayana Ramesh <107012@tfcorp.com>
2022-10-28 17:15:34 +0530

    Lambda Expression and Sorted List

---
+++ FourthProject-28thOct/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FourthProject_28thOct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LambdaExpression lambdaExpression = new LambdaExpression();
+            lambdaExpression.lambda();
+
+            //SortedList sortedList = new SortedList();
+            //sortedList.sorted();
+
+        }
+    }
+}
---
+++ FourthProject-28thOct/SortedList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FourthProject_28thOct
+{
+    
+    public class SortedList
+    {
+        public void sorted()
+        {
+            SortedList<int, int> numberNames = new SortedList<int, int>()
+                                    {
+                                        {3, 1},
+                                        {1, 2},
+                                        {2, 3}
+                                    };
+            for (int i = 0; i < numberNames.Count; i++)
+            {
+                Console.WriteLine("key: {0}, value: {1}", numberNames.Keys[i], numberNames.Values[i]);
+            }
+        }
+    }
+}
+
+//https://www.tutorialsteacher.com/csharp/csharp-collection
